c83d9eb2074abe09a410f3fb4b45e0ec87a20ebd
Dawson Schroeder <schroeder.dawson.d@gmail.com>
2023-05-02 14:04:56 -0500

    "updated exercises for where sql"

--- where_exercises.sql
+++ where_exercises.sql
@@ -3,14 +3,15 @@ USE employees;
 # part 1
 SELECT first_name FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 SELECT last_name FROM employees WHERE last_name LIKE 'E%';
-SELECT last_name FROM employees where last_name like '%q%';
+SELECT last_name FROM employees where last_name like '%Q%';
 
 # part 2
-SELECT first_name FROM employees WHERE first_name = 'Irena' AND gender = 'M'
-    OR first_name = 'Vidya' AND gender = 'M'
-    OR first_name = 'Maya' AND gender = 'M';
+SELECT first_name FROM employees WHERE (first_name = 'Irena'
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya')
+        AND gender = 'M';
 SELECT last_name FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%E';
 SELECT last_name FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
-SELECT last_name FROM employees WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%';
+SELECT last_name FROM employees WHERE last_name LIKE '%Q%' AND last_name NOT LIKE '%QU%';
 
 
