365c2171476323bb839f2415c46e88eaf8085497
Dawson Schroeder <schroeder.dawson.d@gmail.com>
2023-05-03 16:25:02 -0500

    "finished aliases exercise"

---
+++ aliases_exercises.sql
@@ -0,0 +1,11 @@
+USE employees;
+
+# 3:
+SELECT CONCAT(last_name, ' ', first_name) AS full_name FROM employees LIMIT 10;
+
+# 4:
+SELECT CONCAT(last_name, ' ', first_name) AS full_name, DATE(birth_date) AS DOB FROM employees LIMIT 10;
+
+# 5:
+SELECT CONCAT(emp_no, ' - ', last_name, ' ', first_name) AS full_name, DATE(birth_date) AS DOB FROM employees LIMIT 10;
+
